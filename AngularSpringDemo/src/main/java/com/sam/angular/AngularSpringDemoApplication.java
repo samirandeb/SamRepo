ffca545cd49867b573aa076d1098eb1d4c923571
SamiranDeb <samirandeb79@gmail.com>
2018-04-30 16:29:39 +0530

    Angular5 with Maven Build

---
+++ AngularSpringDemo/src/main/java/com/sam/angular/AngularSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.sam.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringDemoApplication.class, args);
+	}
+}
